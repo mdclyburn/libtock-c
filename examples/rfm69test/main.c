fa10237f9ce695b2f9d131c19784f4aeafafa3be
Marshall Clyburn <mardev2013@gmail.com>
2022-07-25 15:23:45 -0400

    Send packets as soon as the radio is ready

--- examples/rfm69test/main.c
+++ examples/rfm69test/main.c
@@ -8,10 +8,19 @@
 
 #define BUFFER_LENGTH 66
 
+bool rts = true;
+
 void fast_blink(void);
 void configure_radio(void);
 
 uint8_t RXTXBuffer[BUFFER_LENGTH];
+
+void on_tx_complete(int _a, int _b, int _c, void* _data) {
+    printf("Packet sent successfully.\n");
+    rts = true;
+
+    return;
+}
 
 int main(void) {
     fast_blink();
@@ -22,22 +31,14 @@ int main(void) {
         RXTXBuffer[0] = 7;
         RXTXBuffer[1] = 44;
         RXTXBuffer[2] = i++;
+
+        delay_ms(1520);
         int r = rfm69_transmit();
-        if (r != 0) {
-            printf("Failed to transmit: %i\n", r);
-            // Stay here. No more attempts.
-            while (true) { delay_ms(10000); }
-        }
+        delay_ms(100);
 
-        delay_ms(6000);
+        while (!rts) { delay_ms(333); }
+        rts = false;
     }
-
-    /* for (uint8_t i = 0; i < BUFFER_LENGTH; i++) { RXTXBuffer[i] = 0x00; } */
-    /* RXTXBuffer[0] = 0x07; */
-    /* RXTXBuffer[1] = 0x44; */
-    /* rfm69_transmit(); */
-    /* delay_ms(3000); */
-    /* while (rfm69_dump_registers(1) != 0) { delay_ms(500); } */
 
     while (true) { delay_ms(3000); }
 }
@@ -81,6 +82,11 @@ void configure_radio(void) {
     /*         printf("RFM69 driver failed to set encryption key (%i).\n", i); */
     /*     } */
     /* } */
+
+    if (rfm69_set_tx_complete_callback(on_tx_complete, NULL) != 0) {
+        printf("RFM69 driver failed to set callback.\n");
+        while (true) { delay_ms(3000); }
+    }
 
     if (rfm69_set_tx_rx_buffer(RXTXBuffer, BUFFER_LENGTH) != 0) {
         printf("RFM69 driver failed to set RX/TX buffer.\n");
