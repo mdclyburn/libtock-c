e7e8987bd723287e0b424c9bf5f55ee66d76f7bf
Marshall Clyburn <mardev2013@gmail.com>
2023-06-14 15:10:19 -0400

    Check return values

--- examples/loudness/main.c
+++ examples/loudness/main.c
@@ -1,23 +1,34 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include <adc.h>
 #include <timer.h>
 
 #define SAMPLE_BUFFER_LEN 512
+#define SAMPLING_PERIOD_MS 7500
 
 tock_timer_t audio_sampling_timer;
 
 bool audio_pending;
 uint16_t samples[SAMPLE_BUFFER_LEN];
 
+void check_return_code(int, const char* const);
+
 void audio_sampling_timer_fired(int, int, int, void*);
 void audio_buffer_filled(uint8_t, uint32_t, uint16_t*, void*);
 
 int main(void)
 {
-    adc_set_buffer(samples, SAMPLE_BUFFER_LEN);
-    adc_set_buffered_sample_callback(audio_buffer_filled, NULL);
+    printf("starting loudness\n");
+    audio_pending = false;
+    check_return_code(
+        adc_set_buffer(samples, SAMPLE_BUFFER_LEN),
+        "adc_set_buffer");
+    check_return_code(
+        adc_set_buffered_sample_callback(audio_buffer_filled, NULL),
+        "adc_set_buffered_sample_callback");
+    timer_every(SAMPLING_PERIOD_MS, audio_sampling_timer_fired, NULL, &audio_sampling_timer);
 
     while (true)
     {
@@ -26,10 +37,22 @@ int main(void)
 
         // Process the audio data.
         // See if it crosses the loudness threshold.
+        printf("processing audio\n");
 
         // Mark the buffer as ready to hold new data.
         audio_pending = false;
     }
+}
+
+void check_return_code(const int rc, const char* const note)
+{
+    if (rc != RETURNCODE_SUCCESS)
+    {
+        printf("loudness: non-zero return code (%s)\n", note);
+        while (true);
+    }
+
+    return;
 }
 
 void audio_sampling_timer_fired(__attribute__ ((unused)) int a1,
@@ -37,12 +60,15 @@ void audio_sampling_timer_fired(__attribute__ ((unused)) int a1,
                                 __attribute__ ((unused)) int a3,
                                 __attribute__ ((unused)) void* a4)
 {
+    printf("sampling timer fired\n");
     const uint8_t channel = 0;
     const uint32_t sampling_frequency = 5120;
 
     if (!audio_pending)
     {
-        adc_buffered_sample(channel, sampling_frequency);
+        check_return_code(
+            adc_buffered_sample(channel, sampling_frequency),
+            "adc_buffered_sample");
     }
 
     return;
