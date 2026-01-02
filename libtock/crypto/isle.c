b962dd44d4e75da2f9df8c9d8bb3cfbecea89669
Marshall Clyburn <mardev2013@gmail.com>
2026-01-02 11:30:28 -0500

    Add functions for indicating IP address

--- libtock/crypto/isle.c
+++ libtock/crypto/isle.c
@@ -29,10 +29,36 @@ libtock_isle_allow_rw_set_out_buffer(
 }
 
 returncode_t
+libtock_isle_command_set_address(
+	const uint8_t* const address)
+{
+	syscall_return_t crval;
+	returncode_t rc;
+
+	crval = command(
+		DRIVER_NUM_ISLE,
+		ISLE_COMMAND_SET_ADDRESS_LOWER,
+		*((uint32_t*) (address + 0)),
+		*((uint32_t*) (address + 4)));
+	rc = tock_command_return_novalue_to_returncode(crval);
+	if (rc != 0) {
+		return rc;
+	}
+
+	crval = command(
+		DRIVER_NUM_ISLE,
+		ISLE_COMMAND_SET_ADDRESS_UPPER,
+		*((uint32_t*) (address + 8)),
+		*((uint32_t*) (address + 12)));
+
+	return tock_command_return_novalue_to_returncode(crval);
+}
+
+returncode_t
 libtock_isle_command_encrypt(
 	const uint32_t message_len)
 {
-	command_return_t crval = command(
+	syscall_return_t crval = command(
 		DRIVER_NUM_ISLE,
 		ISLE_COMMAND_ENCRYPT,
 		message_len,
