e360489a3ea580db7ccc4eaeaf4518e40d802c9c
Marshall Clyburn <mardev2013@gmail.com>
2026-01-23 22:28:02 -0500

    Modify blink to test energy usage

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -1,24 +1,35 @@
+#include <stdio.h>
+
 #include <libtock-sync/services/alarm.h>
 #include <libtock/interface/led.h>
 
 int main(void) {
   // Ask the kernel how many LEDs are on this board.
-  int num_leds;
-  int err = libtock_led_count(&num_leds);
-  if (err < 0) return err;
+  /* int num_leds; */
+  /* int err = libtock_led_count(&num_leds); */
+  /* if (err < 0) return err; */
 
-  // Blink the LEDs in a binary count pattern and scale
-  // to the number of LEDs on the board.
-  for (int count = 0; ; count++) {
-    for (int i = 0; i < num_leds; i++) {
-      if (count & (1 << i)) {
-        libtock_led_on(i);
-      } else {
-        libtock_led_off(i);
-      }
-    }
+  /* // Blink the LEDs in a binary count pattern and scale */
+  /* // to the number of LEDs on the board. */
+  /* for (int count = 0; ; count++) { */
+  /*   for (int i = 0; i < num_leds; i++) { */
+  /*     if (count & (1 << i)) { */
+  /*       libtock_led_on(i); */
+  /*     } else { */
+  /*       libtock_led_off(i); */
+  /*     } */
+  /*   } */
 
-    // This delay uses an underlying alarm in the kernel.
-    libtocksync_alarm_delay_ms(250);
-  }
+	while (true) {
+		uint32_t current;
+		for (uint32_t i = 0; i < 2000; i++) {
+			current = libtock_unsafe_now();
+		}
+
+		printf("current time: %ld\n", current);
+
+		// This delay uses an underlying alarm in the kernel.
+		libtocksync_alarm_delay_ms(500);
+	}
+  /* } */
 }
